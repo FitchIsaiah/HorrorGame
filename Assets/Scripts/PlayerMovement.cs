413143912436310351879bb3569dc1ebd31ed5e5
Isaiah Fitch <112583142+FitchIsaiah@users.noreply.github.com>
2024-11-24 20:36:55 -0800

    Crouching
    Crounching is Done but sprinint mechaninc needs work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -12,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     public float sprintSpeed = 18f;
     public float crouchSpeed = 6f;
     public float gravity = -9.81f;
+
+    public Transform cameraTransform;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -41,12 +44,17 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = -2f; 
         }
+
+        HandleSprinting();
+        HandleCrouching();
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * currentSpeed * Time.deltaTime);
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -58,4 +66,41 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
     }
+    void HandleSprinting()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.C))
+
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = speed;
+        }
+    }
+        void HandleCrouching()
+        {
+            if (Input.GetKey(KeyCode.C))
+            {
+                controller.height = Mathf.Lerp(controller.height, crouchHeight, Time.deltaTime * transitionSpeed);
+                currentSpeed = crouchSpeed;
+
+            //Smoothly lower the camera
+            cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, new Vector3(cameraTransform.localPosition.x, crouchHeight / 2, cameraTransform.localPosition.z), Time.deltaTime * transitionSpeed);
+
+
+
+            }
+            else
+            {
+                controller.height = Mathf.Lerp(controller.height, standHeight, Time.deltaTime * transitionSpeed);
+                currentSpeed = speed;
+
+            cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, new Vector3(cameraTransform.localPosition.x, standHeight / 2, cameraTransform.localPosition.z), Time.deltaTime * transitionSpeed);
+
+        }
+        
+        
+        }
+
 }
